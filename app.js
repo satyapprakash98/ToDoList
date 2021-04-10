15e7249989be3644da7587d496ff9ff4c6adb5a7
Satya prakash <satyapprakash98@gmail.com>
2021-04-10 16:53:19 +0530

    workflow in notes added at this step

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+var items=["buy food","cook food","eat food"];
+app.get("/", function(req, res) {
+  var today = new Date();
+  var currentDay = today.getDay();
+
+  var options = {
+    weekday:"long",
+    day : "numeric",
+    month : "long"
+  };
+    var day = today.toLocaleDateString('en-US',options);
+  res.render('list', {
+    kindOfDay: day,
+    NewListItem:items
+  });
+})
+
+
+app.post('/',function(req,res){
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect('/');
+});
+
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000");
+})
